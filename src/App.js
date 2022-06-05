2492b6e70d744f36a17a2b48e5aefc29f3ac4496
Tomáš <tomas.zifcak197@gmail.com>
2022-06-05 16:19:40 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./sections/Header/Header.js";
+import Footer from "./sections/Footer/Footer.js"
+import Main from "./sections/Main/Main.js";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#AA14F0',
+    },
+    secondary: {
+      main: '#f014c8',
+    },
+    error: {
+      main: '#FF8787',
+    },
+    success: {
+      main: '#87FF93',
+    },
+    warning: {
+      main: '##0aa14',
+    },
+  },
+});
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App" style={{ overflowX: 'hidden' }}>
+        <Header></Header>
+        <Main></Main>
+        <Footer></Footer>
+      </div>
+    </ThemeProvider>
   );
 }
 
